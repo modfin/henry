cc3362e5e0e6d227b3f5ab9fd8a27761205a772b
raz <rasmus.holm@modularfinance.se>
2024-04-24 17:22:43 +0200

    adding zero and coalesce function

--- compare/compare.go
+++ compare/compare.go
@@ -11,6 +11,12 @@ func Compare[N Ordered](a N, b N) int {
 	}
 }
 
+// isNaN reports whether x is a NaN without requiring the math package.
+// This will always return false if T is not floating-point.
+func isNaN[T Ordered](x T) bool {
+	return x != x
+}
+
 // Identity is the identity function for something that is comparable
 func Identity[N comparable](n N) N {
 	return n
@@ -45,6 +51,17 @@ func EqualOf[N comparable](needle N) func(b N) bool {
 	}
 }
 
+// IsZero returns function that looks at the input that returns true if passed in argument is zero
+func IsZero[N comparable]() func(b N) bool {
+	var n N
+	return EqualOf(n)
+}
+
+// IsNotZero returns function that looks at the input that returns true if passed in argument is *Not* zero
+func IsNotZero[N comparable]() func(b N) bool {
+	return NegateOf(IsZero[N]())
+}
+
 // NegateOf returns a function the negates the result of the original function passed in
 func NegateOf[A any](f func(A) bool) func(A) bool {
 	return func(a A) bool {
@@ -58,4 +75,16 @@ func Ternary[A any](boolean bool, ifTrue A, ifFalse A) A {
 		return ifTrue
 	}
 	return ifFalse
+}
+
+// Coalesce returns the first of its arguments that is not equal to the zero value.
+// If no argument is non-zero, it returns the zero value.
+func Coalesce[T comparable](vals ...T) (t T) {
+	zero := IsZero[T]()
+	for _, val := range vals {
+		if !zero(val) {
+			return val
+		}
+	}
+	return t
 }
