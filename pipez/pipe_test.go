c8f41357cb3865ae9f21f1c0adf153e231c017c6
Marcus Widegren <marcus.widegren@modularfinance.se>
2024-11-11 09:10:43 +0100

    Add pipe test that doesn't use Of or Slice methods

--- pipez/pipe_test.go
+++ pipez/pipe_test.go
@@ -11,3 +11,12 @@ func TestPipe_Concat(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestPipe_Concat_withoutWrap(t *testing.T) {
+	var firstSlice Pipe[int] = []int{1, 2, 3}
+	var secondSlice Pipe[int] = []int{4, 5, 6}
+	var res []int = firstSlice.Concat(secondSlice)
+	if !reflect.DeepEqual(res, []int{1, 2, 3, 4, 5, 6}) {
+		t.Fail()
+	}
+}
